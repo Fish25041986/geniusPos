5ee53980a868c0f3cbd3aba3d917beee80e7cc7d
Fish25041986 <oscar-aguirre@hotmail.es>
2024-05-07 09:59:18 -0500

    commit 001

---
+++ src/test/java/com/genius/pos/GeniusPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genius.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeniusPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
